b7dc3562cf3b4dd3940c7927b9bbe9dff2998234
justine <justinewallet1@gmail.com>
2023-04-27 16:34:44 +0200

    corrction

--- ProjetReact/hello-react/hello-react/src/components/Header/HeaderComponent.jsx
+++ ProjetReact/hello-react/hello-react/src/components/Header/HeaderComponent.jsx
@@ -6,7 +6,7 @@ const HeaderComponent = () => {
         <div id="background-green">
             
     <h2>Pottok ♥</h2>
-    <p id="italic">Texte ici</p>
+    <p id="italic">Texte ici test</p>
     <img src="./img/pottok.jpg" id="logocheval" class="logo" alt="logoPottok" />
         </div>
     );
