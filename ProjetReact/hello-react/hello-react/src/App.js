5eea981669e3777bf0a456ab1dbd17a60e694e12
justine <justinewallet1@gmail.com>
2023-04-27 11:21:42 +0200

    jais envie de creuver

--- ProjetReact/hello-react/hello-react/src/App.js
+++ ProjetReact/hello-react/hello-react/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import List from "./List.cs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Board extends React.Component {
+  render() {
+    const lists = [
+      {
+        id: "list-1",
+        title: "List 1",
+        cards: [
+          { id: "card-1", text: "Card 1" },
+          { id: "card-2", text: "Card 2" },
+          { id: "card-3", text: "Card 3" },
+        ],
+      },
+      {
+        id: "list-2",
+        title: "List 2",
+        cards: [
+          { id: "card-4", text: "Card 4" },
+          { id: "card-5", text: "Card 5" },
+          { id: "card-6", text: "Card 6" },
+        ],
+      },
+    ];
+
+    return (
+      <div>
+        <h1>Board</h1>
+        {lists.map((list) => (
+          <List key={list.id} title={list.title} cards={list.cards} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Board;
