92e490f38e66303c4cdcb3380d1165817af20215
thibault sargentini <tsargentini@yahoo.fr>
2023-04-27 16:16:27 +0200

    pitichgmnt

--- ProjetReact/hello-react/hello-react/src/components/Header/HeaderComponent.jsx
+++ ProjetReact/hello-react/hello-react/src/components/Header/HeaderComponent.jsx
@@ -6,7 +6,7 @@ const HeaderComponent = () => {
         <div id="background-green">
             
     <h2>Pottok ♥</h2>
-    <p id="italic">Texte ici</p>
+    <p id="italic">Gagnez la course contre le temps! Démarrez au galop avec notre gestionnaire de tâches intuitif!</p>
     <img src="./img/pottok.jpg" id="logocheval" class="logo" alt="logoPottok" />
         </div>
     );
