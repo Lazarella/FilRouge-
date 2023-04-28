939084e290b0dfbe0034f41af65faf151e62e2b7
thibault sargentini <tsargentini@yahoo.fr>
2023-04-28 16:06:39 +0200

    bodyaccueil

--- ProjetReact/hello-react/hello-react/src/App.js
+++ ProjetReact/hello-react/hello-react/src/App.js
@@ -3,21 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import HeaderComponent from './components/Header/HeaderComponent';
 import FooterComponent from './components/Footer/FooterComponent';
-
+import Body from './components/Body/Body';
 
 function App() {
   return ( 
     <div className="App">
          <HeaderComponent/>
-         {/* <Bodycomponent />
-         <div className="row g-0">
-                <div className="col-4 columns" id='left'>
-                    
-                </div>
-                <div className="col-4 columns" id='right'>
-                    
-                </div>
-        </div> */}
+         <Body />         
          <FooterComponent/>
     </div>
   );
---
+++ ProjetReact/hello-react/hello-react/src/components/Body/Body.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Body.css';
+
+const Body = () => {
+    return (
+        
+ <div className="row">
+                <div className="col" id='Left'>
+                <p>Right</p>
+                </div>
+                <div className="col" id='Right'>
+                <p>Left</p>
+                </div>
+</div>
+        
+    );
+}
+
+export default Body;
