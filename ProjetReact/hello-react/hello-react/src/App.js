fc4175c475171773580106eedea0cfb894babe06
thibault sargentini <tsargentini@yahoo.fr>
2023-04-28 11:24:00 +0200

    yo

--- ProjetReact/hello-react/hello-react/src/App.js
+++ ProjetReact/hello-react/hello-react/src/App.js
@@ -4,10 +4,20 @@ import './App.css';
 import HeaderComponent from './components/Header/HeaderComponent';
 import FooterComponent from './components/Footer/FooterComponent';
 
+
 function App() {
-  return (
+  return ( 
     <div className="App">
          <HeaderComponent/>
+         {/* <Bodycomponent />
+         <div className="row g-0">
+                <div className="col-4 columns" id='left'>
+                    
+                </div>
+                <div className="col-4 columns" id='right'>
+                    
+                </div>
+        </div> */}
          <FooterComponent/>
     </div>
   );
